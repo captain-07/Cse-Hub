555b43ef06094fabbc9e6cd6db420f0c7f77d0a1
Debjyoti <sahadebjyoti363@gmail.com>
2026-02-01 21:57:44 +0530

    setup fix

--- CseHub/views.py
+++ CseHub/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("I am Debjyoti Saha")
+    return render(request, 'index.html')
